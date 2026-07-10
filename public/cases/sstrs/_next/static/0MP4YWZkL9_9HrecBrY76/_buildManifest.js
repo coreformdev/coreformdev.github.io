206e86585992ca2d640d5f0171bdeaff0b98d07e
uebandev <axis-noreply@inbox.ru>
2026-07-10 23:43:28 +0500

    feat: update sstrs case design

--- public/cases/sstrs/_next/static/0MP4YWZkL9_9HrecBrY76/_buildManifest.js
+++ public/cases/sstrs/_next/static/0MP4YWZkL9_9HrecBrY76/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/cases/sstrs//_next/:path+",
-        "destination": "/cases/sstrs/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
